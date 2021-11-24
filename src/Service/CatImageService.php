5c23304c5c04d2cc9d88ed365dfe7a8dba8633b9
Matthias Noback <matthiasnoback@gmail.com>
2021-11-24 11:04:27 +0100

    Assignment 3

--- src/Service/CatImageService.php
+++ src/Service/CatImageService.php
@@ -10,6 +10,25 @@ final class CatImageService
 {
     public function getRandomImage(): ImagesSearchResponse
     {
-        return TheCatApi::imagesSearch();
+        if (rand(0, 9) >= 5) {
+            return TheCatApi::imagesSearch();
+        }
+
+        $images = [
+            '/images/cat1.jpg',
+            '/images/cat2.png'
+        ];
+        $key = array_rand($images);
+        $url = $images[$key];
+
+        return new ImagesSearchResponse(
+            [
+                'id' => '',
+                'width' => 0,
+                'height' => 0,
+                'url' => $url,
+                'breeds' => []
+            ]
+        );
     }
 }
